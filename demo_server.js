55ad06dc39cb2963b077e55253215506c4e15e16
Markus Peroebner <markus.peroebner@gmail.com>
2014-09-23 15:27:11 +0200

    started pop3 server implementation

---
+++ demo_server.js
@@ -0,0 +1,38 @@
+var pop3 = require('./lib/main.js');
+
+function main(){
+    var server = pop3.create_server(function(connection){
+        console.log('Client connected');
+
+        connection.on('authentication', function(user, pass, success){
+            console.log('Authentication requested for ' + user + '/' + pass);
+
+            return success(true);
+        });
+
+        connection.on('stat', function(callback){
+            console.log('Stat requested');
+
+            return callback(null, 1, 236);
+        });
+
+        connection.on('list', function(callback){
+            console.log('List requested');
+
+            return callback(null, [
+                {
+                    message_index: 1,
+                    message_size: 236
+                }
+            ]);
+        });
+    });
+
+    server.listen(110);
+}
+
+function create_command_handler(){
+    // TODO
+}
+
+main();
