78364dfd78d7ef65503df3713d1dce7c490473c2
Markus Peröbner <markus.peroebner@gmail.com>
2014-09-26 19:43:20 +0200

    added test for authentication event

--- test/pop_connection_test.js
+++ test/pop_connection_test.js
@@ -11,10 +11,13 @@ describe('create_pop_connection', function(){
         socket_connection = new socket_mock.SocketMock();
 
         pop_connection = pop3.create_pop_connection(socket_connection);
+
     });
 
     describe('CAPA', function(){
         it('should return empty feature list', function(){
+            clear_server_sent_data();
+
             client_sends_data('CAPA\r\n');
 
             server_sent_data('.\r\n');
@@ -23,9 +26,29 @@ describe('create_pop_connection', function(){
 
     describe('USER', function(){
         it('should accept username and return +OK', function(){
+            clear_server_sent_data();
+
             client_sends_data('USER hanibal\r\n');
 
             server_sent_data('+OK\r\n');
+        });
+    });
+
+    describe('USER and PASS', function(){
+        it('should emit authentication event', function(done){
+            var expected_user = 'hanibal';
+            var expected_password = '42';
+
+            pop_connection.on('authentication', function(user, password, callback){
+                user.should.equal(expected_user);
+                password.should.equal(expected_password);
+
+                done();
+            });
+
+            client_sends_data('USER ' + expected_user + '\r\n');
+
+            client_sends_data('PASS ' + expected_password + '\r\n');
         });
     });
 
@@ -34,6 +57,10 @@ describe('create_pop_connection', function(){
     }
 
     function server_sent_data(s){
-        socket_connection.written.should.equal(server_hello + s);
+        socket_connection.written.should.equal(s);
+    }
+
+    function clear_server_sent_data(){
+        socket_connection.written = '';
     }
 });
